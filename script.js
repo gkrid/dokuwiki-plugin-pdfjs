4ed465bc987f32e8eeb4f6303cd87b13fa286b2f
Alexander Tse <tsetsealexander@gmail.com>
2020-06-14 15:40:29 -0700

    Add 4 second delay to ensure buttons are hidden on slow networks.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-jQuery(function () {
-    var $iframes = jQuery('.plugin__pdfjs');
-    $iframes.contents().find('#openFile').hide();
+jQuery(document).ready(function(){
+	function remove_buttons() {
+		var $iframes = jQuery('.plugin__pdfjs');
+		$iframes.contents().find('#openFile').remove();
 
-    if (JSINFO['plugin_pdfjs']['hide_download_button'] === 1) {
-        $iframes.contents().find('#download').hide();
-    }
+		if (JSINFO['plugin_pdfjs']['hide_download_button'] === 1) {
+			$iframes.contents().find('#download').remove();
+		}
+	}
+	setTimeout(remove_buttons,4000);
 });
