660d25daf6c03081f8f4dea1a00526ccc37cb348
Alexander Tse <tsetsealexander@gmail.com>
2020-06-14 16:29:28 -0700

    Bug Fix - Remove buttons when options are pushed to Secondary Buttons on smaller screens

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ jQuery(document).ready(function(){
 	function remove_buttons() {
 		var $iframes = jQuery('.plugin__pdfjs');
 		$iframes.contents().find('#openFile').remove();
-
+    $iframes.contents().find('#secondaryOpenFile').remove();
 		if (JSINFO['plugin_pdfjs']['hide_download_button'] === 1) {
 			$iframes.contents().find('#download').remove();
+      $iframes.contents().find('#secondaryDownload').remove();
 		}
 	}
 	setTimeout(remove_buttons,4000);
