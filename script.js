1dd13ac5ea93aaeecd6ffa5d55a0ed6c4946ef27
Szymon Olewniczak <solewniczak@rid.pl>
2020-06-26 11:42:55 +0200

    fix idention, run remove_buttons twice for fast and slower networks

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-jQuery(document).ready(function(){
-	function remove_buttons() {
-		var $iframes = jQuery('.plugin__pdfjs');
-		$iframes.contents().find('#openFile').remove();
-    $iframes.contents().find('#secondaryOpenFile').remove();
-		if (JSINFO['plugin_pdfjs']['hide_download_button'] === 1) {
-			$iframes.contents().find('#download').remove();
-      $iframes.contents().find('#secondaryDownload').remove();
-		}
-	}
-	setTimeout(remove_buttons,4000);
+jQuery(document).ready(function () {
+    function remove_buttons() {
+        var $iframes = jQuery('.plugin__pdfjs');
+        $iframes.contents().find('#openFile').remove();
+        $iframes.contents().find('#secondaryOpenFile').remove();
+        if (JSINFO['plugin_pdfjs']['hide_download_button'] === 1) {
+            $iframes.contents().find('#download').remove();
+            $iframes.contents().find('#secondaryDownload').remove();
+        }
+    }
+    remove_buttons()
+    // do it once again after 4 seconds for slower networks -> refer #18
+    setTimeout(remove_buttons, 4000);
 });
